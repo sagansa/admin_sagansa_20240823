78dd66c70469395bbd0c2d490d47726989b3b850
sagansa <asapanganbangsa@gmail.com>
2024-09-09 00:10:21 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -78,9 +78,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make('super_admin')
             ]);
+            // ->plugins([
+            //     \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make('super_admin')
+            // ]);
     }
 }
