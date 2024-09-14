56be48ab862b948e7b8b2048e7654f8f6b188961
sagansa <asapanganbangsa@gmail.com>
2024-09-14 09:14:14 +0700

    Update InvoicePurchaseResource.php

--- app/Filament/Resources/Panel/InvoicePurchaseResource.php
+++ app/Filament/Resources/Panel/InvoicePurchaseResource.php
@@ -235,7 +235,7 @@ class InvoicePurchaseResource extends Resource
 
                             $paymentTypeFilter = '';
                                 if ($paymentTypeId == '1') { // transfer
-                                    $paymentTypeFilter = ['1', '2']; // transfer dan tunai
+                                    $paymentTypeFilter = ''; // transfer dan tunai
                                 } elseif ($paymentTypeId == '2') { // tunai
                                     $paymentTypeFilter = '2'; // transfer
                             }
