9c3eb17381de7b6aa66d7760fbf551800b8fbfd0
sagansa <asapanganbangsa@gmail.com>
2024-09-17 07:03:11 +0700

    Update ListPaymentReceipts.php

--- app/Filament/Resources/Panel/PaymentReceiptResource/Pages/ListPaymentReceipts.php
+++ app/Filament/Resources/Panel/PaymentReceiptResource/Pages/ListPaymentReceipts.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Panel\PaymentReceiptResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\Panel\PaymentReceiptResource;
+use Filament\Resources\Components\Tab;
 
 class ListPaymentReceipts extends ListRecords
 {
@@ -13,5 +14,15 @@ class ListPaymentReceipts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [Actions\CreateAction::make()];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'fuel service' => Tab::make()->query(fn ($query) => $query->where('status', '1')),
+            'daily salary' => Tab::make()->query(fn ($query) => $query->where('status', '2')),
+            'invoice' => Tab::make()->query(fn ($query) => $query->where('status', '3')),
+        ];
     }
 }
