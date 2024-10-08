ad34c818b4ed86405dfbb8ee4a6ac586af2403b3
sagansa <asapanganbangsa@gmail.com>
2024-10-08 23:45:49 +0700

    update

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -37,7 +37,16 @@
             <!-- Login and Register on the right -->
             @auth
                 <div class="hidden space-x-4 lg:flex">
-                    <a href="https://www.sagansa.id/admin" class="px-4 py-2 text-white rounded bg-green">Shop</a>
+                    <a href="https://www.sagansa.id/admin" class="flex items-center px-4 py-2 text-white rounded bg-green">
+                        <!-- SVG Cart Icon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+
+                        <div class="mx-2">Shop</div>
+                    </a>
                 </div>
             @else
                 <div class="hidden space-x-4 lg:flex">
