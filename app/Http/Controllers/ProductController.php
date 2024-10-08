47679c9da412a2d752fc303ad12ae221445b5373
sagansa <asapanganbangsa@gmail.com>
2024-10-08 23:23:13 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        // $contact = Contact::first();
-        $products = Product::where('online_category_id', '<>', '4')->get();
-        // return view('landing-page', compact('contact', 'products'));
-        return view('home', compact('products'));
-    }
+    // public function index()
+    // {
+    // $contact = Contact::first();
+    // $products = Product::where('online_category_id', '<>', '4')->get();
+    // return view('landing-page', compact('contact', 'products'));
+    // return view('home', compact('products'));
+    // }
 }
