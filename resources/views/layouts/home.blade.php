27a316302224cd20a7db8cfb92b68178a2c506e3
sagansa <asapanganbangsa@gmail.com>
2024-10-08 23:39:28 +0700

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,7 +13,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
+
 
 </head>
 
