d9d782b8e3cacaef702e52808184158375f3329b
sagansa <asapanganbangsa@gmail.com>
2024-10-09 13:52:31 +0700

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -110,7 +110,16 @@
             <a href="#products" class="block px-4 py-2 text-green">Produk Kami</a>
             <a href="#contact" class="block px-4 py-2 text-green">Hubungi Kami</a>
             @auth
-                <a href="https://www.sagansa.id/admin" class="block px-4 py-2 text-white bg-green">Login</a>
+                <a href="https://www.sagansa.id/admin" class="flex items-center px-4 py-2 text-white rounded-lg bg-green">
+                    <!-- SVG Cart Icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+
+                    <div class="mx-2">Shop</div>
+                </a>
             @else
                 <a href="https://www.sagansa.id/admin/login" class="block px-4 py-2 text-white bg-green">Login</a>
                 <a href="https://www.sagansa.id/admin/register"
