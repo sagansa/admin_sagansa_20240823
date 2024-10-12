2d9cc7ff9faac014461baadccf53b8a8423681ff
sagansa <asapanganbangsa@gmail.com>
2024-10-12 23:19:52 +0700

    update home

--- resources/views/layouts/home/nav.blade.php
+++ resources/views/layouts/home/nav.blade.php
@@ -1,3 +1,7 @@
+@php
+    use Filament\Facades\Filament;
+@endphp
+
 <nav class="fixed z-10 w-full p-4 shadow-md bg-cream">
     <div class="container flex items-center justify-between mx-auto">
         <!-- Logo on the left -->
@@ -16,7 +20,7 @@
         </div>
 
         <!-- Login and Register on the right -->
-        @auth
+        {{-- @auth
             <div class="hidden space-x-4 lg:flex">
                 <a href="https://www.sagansa.id/admin" class="flex items-center px-4 py-2 text-white rounded-lg bg-green">
                     <!-- SVG Cart Icon -->
@@ -31,11 +35,32 @@
             </div>
         @else
             <div class="hidden space-x-4 lg:flex">
-                <a href="https://www.sagansa.id/admin/login" class="px-4 py-2 text-white rounded-lg bg-green">Login</a>
+                <a href="{{ Filament::getLoginUrl() }}" class="px-4 py-2 text-white rounded-lg bg-green">Login</a>
                 <a href="https://www.sagansa.id/admin/register"
                     class="px-4 py-2 bg-white border-2 rounded-lg text-green border-green">Register</a>
             </div>
-        @endauth
+        @endauth --}}
+
+        @guest
+            <div class="hidden space-x-4 lg:flex">
+                <a href="{{ Filament::getLoginUrl() }}" class="px-4 py-2 text-white rounded-lg bg-green">Login</a>
+                <a href="{{ Filament::getRegistrationUrl() }}"
+                    class="px-4 py-2 bg-white border-2 rounded-lg text-green border-green">Register</a>
+            </div>
+        @else
+            <div class="hidden space-x-4 lg:flex">
+                <a href="{{ Filament::getUrl() }}" class="flex items-center px-4 py-2 text-white rounded-lg bg-green">
+                    <!-- SVG Cart Icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+
+                    <div class="mx-2">Shop</div>
+                </a>
+            </div>
+        @endguest
 
 
         <!-- Hamburger Menu (visible on small screens) -->
@@ -57,7 +82,7 @@
         <a href="#products" class="block px-4 py-2 text-green">Produk Kami</a>
         <a href="#contact" class="block px-4 py-2 text-green">Hubungi Kami</a>
         <a href="https://ev.sagansa.id" class="block px-4 py-2 font-bold text-green">EV</a>
-        @auth
+        {{-- @auth
             <a href="https://www.sagansa.id/admin" class="flex items-center px-4 py-2 text-white rounded-lg bg-green">
                 <!-- SVG Cart Icon -->
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -72,6 +97,21 @@
             <a href="https://www.sagansa.id/admin/login" class="block px-4 py-2 text-white rounded-t-lg bg-green">Login</a>
             <a href="https://www.sagansa.id/admin/register"
                 class="block px-4 py-2 bg-white border-2 rounded-b-lg text-green border-green">Register</a>
-        @endauth
+        @endauth --}}
+        @guest
+            <a href="{{ Filament::getLoginUrl() }}" class="block px-4 py-2 text-white rounded-t-lg bg-green">Login</a>
+            <a href="{{ Filament::getRegistrationUrl() }}"
+                class="block px-4 py-2 bg-white border-2 rounded-b-lg text-green border-green">Register</a>
+        @else
+            <a href="{{ Filament::getUrl() }}" class="flex items-center px-4 py-2 text-white rounded-lg bg-green">
+                <!-- SVG Cart Icon -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+                <div class="mx-2">Shop</div>
+            </a>
+        @endguest
     </div>
 </nav>
