baa2802202f0838031eb09a1d8ba0c353513e7d4
sagansa <asapanganbangsa@gmail.com>
2024-11-05 22:38:34 +0700

    update

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,10 +13,16 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    <link href="{{ asset('build/assets/app-B6OgaiA8.css', 'build/assets/app-BW9_hxub.js') }}" rel="stylesheet">
+    @if (app()->environment('local'))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
 
-    <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+        <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
+    @endif
 
     <style>
         html {
--- routes/api.php
+++ routes/api.php
@@ -10,35 +10,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::get('/user-presence', [PresenceController::class, 'getUserPresence']);
-    Route::post('/check-in', [PresenceController::class, 'checkIn']);
-    Route::post('/check-out', [PresenceController::class, 'checkOut']);
-    Route::get('/stores', [PresenceController::class, 'getStores']);
-    Route::get('/shift-stores', [PresenceController::class, 'getShiftStores']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::prefix('leaves')->group(function () {
-        Route::get('/', [LeaveController::class, 'index']);
-        Route::post('/', [LeaveController::class, 'store']);
-        Route::get('/{id}', [LeaveController::class, 'show']);
-        Route::put('/{id}', [LeaveController::class, 'update']);
-        Route::delete('/{id}', [LeaveController::class, 'destroy']);
-    });
-
-    // Route untuk salary
-    Route::prefix('salary')->group(function () {
-        // Get salary calculation
-        Route::get('/calculate', [SalaryController::class, 'getMySalary']);
-
-        // Get current rate
-        Route::get('/rate', [SalaryController::class, 'getCurrentRate']);
-
-        // Get monthly salaries list
-        Route::get('/monthly', [SalaryController::class, 'getMonthlySalaries']);
-    });
-});
