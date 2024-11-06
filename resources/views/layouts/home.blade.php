e40bad0f02ea9478e267dd1e69c7f748e4baac7d
sagansa <asapanganbangsa@gmail.com>
2024-11-06 22:02:59 +0700

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -16,12 +16,8 @@
     @if (app()->environment('local'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-        @php
-            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-        @endphp
-
-        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
-        <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+        <script src="{{ asset('build/assets/app.js') }}" defer></script>
     @endif
 
     <style>
