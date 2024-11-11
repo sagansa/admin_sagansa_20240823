0eeef340efda0b9739b21c698893cab14e2ad387
sagansa <asapanganbangsa@gmail.com>
2024-11-11 12:30:01 +0700

    Update ImageHelper.php

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -4,9 +4,16 @@ namespace App\Helpers;
 
 class ImageHelper
 {
-    public static function getImageUrl($path)
+    public static function getImageUrl($path, $thumbnail = false)
     {
         $apiUrl = config('services.api.url', 'https://api.sagansa.id');
-        return $apiUrl . '/storage/' . $path;
+        $baseUrl = $apiUrl . '/storage/';
+
+        if ($thumbnail) {
+            // Tambahkan prefix thumbnail jika diperlukan
+            return $baseUrl . 'thumbnails/' . $path;
+        }
+
+        return $baseUrl . $path;
     }
 }
