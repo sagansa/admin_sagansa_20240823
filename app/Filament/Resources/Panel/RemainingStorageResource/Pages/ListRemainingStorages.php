37958bda14f84123d84e46f9aafac8956fc5e0eb
sagansa <83310652+sagansa@users.noreply.github.com>
2025-01-01 21:47:27 +0700

    Update ListRemainingStorages.php

--- app/Filament/Resources/Panel/RemainingStorageResource/Pages/ListRemainingStorages.php
+++ app/Filament/Resources/Panel/RemainingStorageResource/Pages/ListRemainingStorages.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Panel\RemainingStorageResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\Panel\RemainingStorageResource;
+use Carbon\Carbon;
 use Filament\Resources\Components\Tab;
 
 class ListRemainingStorages extends ListRecords
@@ -13,7 +14,15 @@ class ListRemainingStorages extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [Actions\CreateAction::make()];
+        return [Actions\CreateAction::make()
+            ->visible(fn () => $this->isWithinAllowedHours()),
+        ];
+    }
+
+    protected function isWithinAllowedHours(): bool
+    {
+        $currentHour = Carbon::now()->hour; // Pastikan timezone Anda sudah disesuaikan
+        return $currentHour >= 21 && $currentHour <= 8; // Jam 08:00 - 18:00
     }
 
     public function getTabs(): array
